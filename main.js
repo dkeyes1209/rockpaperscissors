e35adfbe568a9021b6bc72f90b08d749d985fd50
Danielle <dkeyes1209@gmail.com>
2023-02-28 12:11:54 -0800

    Add java file

---
+++ main.js
@@ -0,0 +1,21 @@
+const choices = ["rock","paper","scissors"]
+
+function game() {
+    //play the game
+    //play five rounds
+    //console based
+}
+
+function playRound() {
+    const playerSelection = 
+}
+
+function playerChoice () {
+    //get input from player
+}
+
+function computerChoice () {
+    //get random input from computer
+    return choices[Math.floor(Math.random()*choices.length)]
+}
+
